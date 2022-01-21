51ca8e44dbea87bb8422c43dfa6ca767929fecdb
Neidz <68917960+Neidz@users.noreply.github.com>
2022-01-21 23:21:37 +0100

    commands are now displayed correctly in commands tab

--- src/components/commandsTab.tsx
+++ src/components/commandsTab.tsx
@@ -1,3 +1,34 @@
+import "../styles/sections/commandsTab.scss";
+import { useSelector } from "react-redux";
+import { RootState } from "../store/store";
+import { referenceColorsState } from "../store/features/renderVisualsSlice";
+
 export const CommandsTab = () => {
-    return <div className="commandsTab"></div>;
+    const commands = useSelector((state: RootState) => state.commands.commands);
+    const referenceColors = useSelector((state: RootState) => state.renderVisuals.referenceColors);
+
+    return (
+        <div className="commandsTab">
+            <div className="commandList">
+                {commands.map((object: object, key: number) => (
+                    <div className="highlightedCommand" key={key}>
+                        <h4 className="commandNumber">{`command${key + 1}:`}</h4>
+                        {Object.keys(object).map((key: string) => (
+                            <div className="singleAngle" key={key}>
+                                <h4
+                                    className="highlightedAngle"
+                                    style={{
+                                        color: referenceColors[
+                                            `referenceColor${key.replace("angle", "")}` as keyof referenceColorsState
+                                        ],
+                                    }}
+                                >{`${key}:`}</h4>
+                                <h4 className="highlightedValue"> {`${object[key as keyof object]}, `}</h4>
+                            </div>
+                        ))}
+                    </div>
+                ))}
+            </div>
+        </div>
+    );
 };
