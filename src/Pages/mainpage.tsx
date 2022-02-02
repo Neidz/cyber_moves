290046441ecab40b14a644d0b0b43ef8f225ff1d
Neidz <68917960+Neidz@users.noreply.github.com>
2022-02-02 22:39:32 +0100

    mainpage model added, still need to add camera movement

--- src/Pages/mainpage.tsx
+++ src/Pages/mainpage.tsx
@@ -1,5 +1,23 @@
+import { OrbitControls } from "@react-three/drei";
+import { Canvas } from "@react-three/fiber";
+import { Suspense } from "react";
+import { MainpageScene } from "../modelFiles/mainpageScene";
 import "../styles/sections/mainpage.scss";
 
 export const Mainpage = () => {
-    return <div className="mainpage"></div>;
+    return (
+        <div className="mainpage">
+            <div className="mainContainer">
+                <div className="mainRender">
+                    <Canvas shadows>
+                        <ambientLight intensity={0.7} />
+                        <OrbitControls />
+                        <Suspense fallback={null}>
+                            <MainpageScene />
+                        </Suspense>
+                    </Canvas>
+                </div>
+            </div>
+        </div>
+    );
 };
